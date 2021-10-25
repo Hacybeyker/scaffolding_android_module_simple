2dca732765a2a6f60e2160cb90859c55e671fa13
Carlos Osorio <hacybeyker@gmail.com>
2021-10-25 17:01:25 -0500

    se modifico el archivo ConfigureApp.kt

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -1,10 +1,12 @@
 object ConfigureApp {
     //TODO change urlRepoDependencies with your urlRepoDependencies name
-    const val urlRepoDependencies = "https://maven.pkg.github.com/Hacybeyker/app-android-module"
+    const val urlRepoDependencies = "https://maven.pkg.github.com/Hacybeyker/your_path_module"
     //TODO change organization with your organization name
     const val organization = "hacybeyker"
+    //TODO change organization with your organization name
+    const val projectName = "app-android-module"
     //TODO change applicationId with your module name
-    const val applicationId = "app-android-module"
+    const val applicationId = "app.android.module"
     //TODO change groupId with your groupId name
     const val groupId = "com.hacybeyker.android"
     //TODO change artifactId with your module name
