dc965ad7caffe988c4639bd68ea71c98aae3345a
Carlos Osorio <hacybeyker@gmail.com>
2021-11-30 19:43:33 -0500

    se modifico el nombre de un TODO

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -3,7 +3,7 @@ object ConfigureApp {
     const val urlRepoDependencies = "https://maven.pkg.github.com/Hacybeyker/your_path_module"
     //TODO change organization with your organization name
     const val organization = "hacybeyker"
-    //TODO change organization with your organization name
+    //TODO change projectName with your project name
     const val projectName = "app-android-module"
     //TODO change applicationId with your module name
     const val applicationId = "app.android.module"
